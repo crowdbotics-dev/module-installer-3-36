fa7cd4acfbac4a9ce95e8f63ee0eae37c5c23e6e
Dmitrii Alferev <dmitry@alferev.com>
2023-12-26 17:02:58 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "module-installer-3-36",
-  url: "https://module-installer-3-36.botics.co",
-  api: "https://module-installer-3-36.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "module-installer-3-36",
+  "url": "https://module-installer-3-36.botics.co",
+  "api": "https://module-installer-3-36.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
